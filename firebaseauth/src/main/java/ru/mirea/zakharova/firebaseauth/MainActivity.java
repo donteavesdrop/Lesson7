37793830856d4682a0b7a1d6ce663784c539bd4f
donteavesdrop <import_math@mail.ru>
2023-05-28 15:51:26 +0300

    init

--- firebaseauth/src/main/java/ru/mirea/zakharova/firebaseauth/MainActivity.java
+++ firebaseauth/src/main/java/ru/mirea/zakharova/firebaseauth/MainActivity.java
@@ -22,14 +22,17 @@ import ru.mirea.zakharova.firebaseauth.databinding.ActivityMainBinding;
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = MainActivity.class.getSimpleName();
     private ActivityMainBinding binding;
+    // START declare_auth
     private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // Initialization views
         setContentView(R.layout.activity_main);
         FirebaseApp.initializeApp(this);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        // [START initialize_auth] Initialize Firebase Auth
         mAuth = FirebaseAuth.getInstance();
         binding.verifyEmailButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -44,13 +47,17 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         binding.verifyEmailButton.setVisibility(View.GONE);
+        // [END initialize_auth]
     }
+    // [START on_start_check_user]
     @Override
     public void onStart() {
         super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
         FirebaseUser currentUser = mAuth.getCurrentUser();
         updateUI(currentUser);
     }
+    // [END on_start_check_user]
     private void updateUI(FirebaseUser user) {
         if (user != null) {
             binding.textView2.setText(getString(R.string.emailpassword_status_fmt,
@@ -80,15 +87,21 @@ public class MainActivity extends AppCompatActivity {
     }
     private void createAccount(String email, String password) {
         Log.d(TAG, "createAccount:" + email);
+        if (!validateForm()) {
+            return;
+        }
+        // [START create_user_with_email]
         mAuth.createUserWithEmailAndPassword(email, password)
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
                             Log.d(TAG, "createUserWithEmail:success");
                             FirebaseUser user = mAuth.getCurrentUser();
                             updateUI(user);
                         } else {
+                            // If sign in fails, display a message to the user.
                             Log.w(TAG, "createUserWithEmail:failure",
                                     task.getException());
                             Toast.makeText(MainActivity.this, "Authentication failed.",
@@ -97,31 +110,36 @@ public class MainActivity extends AppCompatActivity {
                         }
                     }
                 });
+        // [END create_user_with_email]
     }
     private void signIn(String email, String password) {
         Log.d(TAG, "signIn:" + email);
+        // [START sign_in_with_email]
         mAuth.signInWithEmailAndPassword(email, password)
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
-                            binding.verifyEmailButton.setVisibility(View.VISIBLE);
+                            // Sign in success, update UI with the signed-in user's information
                             Log.d(TAG, "signInWithEmail:success");
+                            binding.verifyEmailButton.setVisibility(View.VISIBLE);
                             FirebaseUser user = mAuth.getCurrentUser();
                             updateUI(user);
                         } else {
+                            // If sign in fails, display a message to the user.
                             Log.w(TAG, "signInWithEmail:failure", task.getException());
                             Toast.makeText(MainActivity.this, "Authentication failed.",
                                     Toast.LENGTH_SHORT).show();
                             updateUI(null);
                         }
-
+                        // [START_EXCLUDE]
                         if (!task.isSuccessful()) {
                             binding.textView2.setText(R.string.auth_failed);
                         }
-
+                        // [END_EXCLUDE]
                     }
                 });
+        //[END sign_in_with_email]
     }
     private void signOut() {
         mAuth.signOut();
@@ -129,12 +147,17 @@ public class MainActivity extends AppCompatActivity {
         updateUI(null);
     }
     private void sendEmailVerification() {
+        // Disable button
         binding.verifyEmailButton.setEnabled(false);
+        // Send verification email
+        // [START send_email_verification]
         final FirebaseUser user = mAuth.getCurrentUser();
         Objects.requireNonNull(user).sendEmailVerification()
                 .addOnCompleteListener(this, new OnCompleteListener<Void>() {
                     @Override
                     public void onComplete(@NonNull Task<Void> task) {
+                        // [START_EXCLUDE]
+                        // Re-enable button
                         binding.verifyEmailButton.setEnabled(true);
                         if (task.isSuccessful()) {
                             Toast.makeText(MainActivity.this,
@@ -146,7 +169,33 @@ public class MainActivity extends AppCompatActivity {
                                     "Failed to send verification email.",
                                     Toast.LENGTH_SHORT).show();
                         }
+                        // [END_EXCLUDE]
                     }
                 });
+        // [END send_email_verification]
+    }
+    private boolean validateForm() {
+        String email = binding.editTextTextPersonName.getText().toString();
+        String password = binding.editTextTextPersonName2.getText().toString();
+
+        if (email.isEmpty()) {
+            binding.editTextTextPersonName.setError("Required.");
+            return false;
+        }
+
+        if (password.isEmpty()) {
+            binding.editTextTextPersonName2.setError("Required.");
+            return false;
+        }
+
+        if (password.length() < 6) {
+            binding.editTextTextPersonName2.setError("Password should be at least 6 characters.");
+            return false;
+        }
+
+        binding.editTextTextPersonName.setError(null);
+        binding.editTextTextPersonName2.setError(null);
+
+        return true;
     }
 }
